3a35d0148eb15c998d5170632ff8a8ffb11af829
Saayam Mehmood <mehmoodsi@cardiff.ac.uk>
2025-11-18 14:31:52 +0000

    Initial commit

---
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/ClientProjectTeam3Application.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.spring.clientprojectteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientProjectTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientProjectTeam3Application.class, args);
+    }
+
+}
