81de746cfb662c3619729d1f864823cb5444bb9d
Sam <HancockS6@cardiff.ac.uk>
2025-11-20 17:41:52 +0000

    Implemented next and previous question navigation, using HTTPSession to store user answers and auto-fill when returning to a question.

--- src/main/java/uk/ac/cf/spring/clientprojectteam3/quiz/QuizRepository.java
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/quiz/QuizRepository.java
@@ -1,5 +1,13 @@
 package uk.ac.cf.spring.clientprojectteam3.quiz;
 
-public interface QuizRepository {
-    QuizDTO getQuiz();
+import java.util.List;
+
+public interface QuizRepo {
+    List<Quiz> getQuizNames();
+    List<Question> getQuestions(long quizId);
+    List<Answers> getAnswers(long quizId, long userId);
+    void addAnswer(long quizId, long userId, int attemptNumber, long questionId, int quizScore);
+    Quiz getQuiz(long quizId);
+
+    void setRowMappers();
 }
