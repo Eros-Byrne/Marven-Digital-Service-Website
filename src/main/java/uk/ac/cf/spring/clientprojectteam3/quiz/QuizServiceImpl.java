f4e6f84c76fb591dc98c5ff1524e2818d2fac9a0
Sam <HancockS6@cardiff.ac.uk>
2025-11-20 17:46:43 +0000

    updated Answer Class

--- src/main/java/uk/ac/cf/spring/clientprojectteam3/quiz/QuizServiceImpl.java
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/quiz/QuizServiceImpl.java
@@ -3,16 +3,21 @@ package uk.ac.cf.spring.clientprojectteam3.quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class QuizServiceImpl implements QuizService {
     @Autowired
     private QuizRepository quizRepository;
 
+
     @Override
-    public QuizDTO getQuizForAttempt(int attemptId) {
+    public Quiz getQuizForAttempt(int quizId, int attemptId) {
+        Quiz quiz = quizRepository.getQuiz(quizId);
+        quiz.setQuizId(quizId);
+        List<Question> quizQuestions = quizRepository.getQuestions(quizId);
+        quiz.setQuestions(quizQuestions);
 
-        // TODO: consider db design when implementing.
-
-        return quizRepository.getQuiz();
+        return quiz;
     }
 }
