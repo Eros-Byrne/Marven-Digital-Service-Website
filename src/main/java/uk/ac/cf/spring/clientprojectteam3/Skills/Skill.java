c893f4472bf5c91f91ce741188183fb6865ceffb
Saayam Mehmood <mehmoodsi@cardiff.ac.uk>
2025-11-20 10:30:38 +0000

    Tested the full stack and separately tested service

--- src/main/java/uk/ac/cf/spring/clientprojectteam3/Skills/Skill.java
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/Skills/Skill.java
@@ -2,9 +2,11 @@ package uk.ac.cf.spring.clientprojectteam3.Skills;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Skill {
     private Long id;
     private String name;
