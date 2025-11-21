44335f5c9d56923e5995f615b53501f55038070b
Sam <HancockS6@cardiff.ac.uk>
2025-11-21 14:22:55 +0000

    addressed merge request comments. reformatted buttons, included navbar and removed duplicate classes.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -31,5 +31,5 @@ values (1,'test question 1 quiz 1', 'how do you feel about skill 1', 1);
 insert into quiz_questions (quiz_id, title, text, skill_id)
 values (1,'test question 2 quiz 1', 'how do you feel about skill 2', 2);
 insert into quiz_questions (quiz_id, title, text, skill_id)
-values (2,'test question 1 quiz 2', 'how do you feel about skill 1 quiz 2', 1);
+values (1,'test question 1 quiz 2', 'how do you feel about skill 1 quiz 2', 1);
 
