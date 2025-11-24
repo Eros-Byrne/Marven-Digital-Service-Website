340132e4339e8ee648bb4685bf81be44a3f084a9
Saayam Mehmood <mehmoodsi@cardiff.ac.uk>
2025-11-24 13:42:28 +0000

    Added error handling to capabilites url

--- src/main/java/uk/ac/cf/spring/clientprojectteam3/Capabilities/CapabilityController.java
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/Capabilities/CapabilityController.java
@@ -4,9 +4,11 @@ import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.clientprojectteam3.Skills.Skill;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 @Controller
@@ -20,14 +22,21 @@ public class CapabilityController {
 
     @GetMapping("/capability/{id}")
     public ModelAndView getCapability(@PathVariable Long id) {
-        ModelAndView mv = new ModelAndView("capabilities/single_capability");
-        Capability capability = capabilityService.getCapability(id);
-        mv.addObject("capability", capability);
-        List<Resource> resources = capabilityService.getResources(id);
-        mv.addObject("resources", resources);
-        List<Skill> skills = capabilityService.getSkills(id);
-        mv.addObject("skills", skills);
-        return mv;
+        ModelAndView mv;
+
+        try {
+            mv = new ModelAndView("capabilities/single_capability");
+            Capability capability = capabilityService.getCapability(id);
+            mv.addObject("capability", capability);
+            List<Resource> resources = capabilityService.getResources(id);
+            mv.addObject("resources", resources);
+            List<Skill> skills = capabilityService.getSkills(id);
+            mv.addObject("skills", skills);
+            return mv;
+        } catch (IllegalArgumentException e) {
+            mv = new ModelAndView("redirect:/outcomes");
+            return mv;
+        }
     }
 
 }
