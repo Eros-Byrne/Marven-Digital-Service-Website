0b3fa82113d7d6bd80b92d3944f1ca0b851ddefa
Ashil Shaju <shajua1@cardiff.ac.uk>
2025-11-24 12:56:14 +0000

    completing fix

--- src/main/java/uk/ac/cf/spring/clientprojectteam3/Skills/Skill.java
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/Skills/Skill.java
@@ -4,10 +4,3 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Skill {
-    private Long id;
-    private String name;
-}
