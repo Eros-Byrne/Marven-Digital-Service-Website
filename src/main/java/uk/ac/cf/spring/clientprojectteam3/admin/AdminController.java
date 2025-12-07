0f83639d758448cc57215cad071e2b45fd9e6c04
Saayam Mehmood <mehmoodsi@cardiff.ac.uk>
2025-12-07 20:53:38 +0000

    Created basic layout of the outcomes list page

--- src/main/java/uk/ac/cf/spring/clientprojectteam3/admin/AdminController.java
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/admin/AdminController.java
@@ -1,4 +1,6 @@
 package uk.ac.cf.spring.clientprojectteam3.admin;
 
 public class AdminController {
+
+    // possibly link to admin dashboard in here - currently unsure what that would look like
 }
