3cc595d9f8ae8b589d89563753d077e0af6d1d96
Saayam Mehmood <mehmoodsi@cardiff.ac.uk>
2025-12-07 16:55:49 +0000

    Added the backend for viewing all outcomes

---
+++ src/main/java/uk/ac/cf/spring/clientprojectteam3/admin/outcome/AdminOutcome.java
@@ -0,0 +1,14 @@
+package uk.ac.cf.spring.clientprojectteam3.admin.outcome;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminOutcome {
+
+    private String title;
+    private Integer capabilityCount;
+}
